cbd3e846f1ae8f3033f0bf9edfbb8f076b3f10c4
alocarin <alocarin@gmail.com>
2025-06-10 19:24:42 -0300

    cambios

---
+++ Pre-enterga-Ecommerce-TT-main/Pre-enterga-Ecommerce-TT-main/pre-entrega-tt/src/context/CartContext.jsx
@@ -0,0 +1,65 @@
+import { createContext, useState, useEffect } from "react";
+
+export const CartContext = createContext()
+
+export const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState([])
+    const [productos, setProductos] = useState([])
+    const [cargando, setCargando] = useState(true)
+    const [error, setError] = useState(false)
+    const [isAuthenticated, setIsAuth] = useState(false)
+
+    useEffect(() => {
+        fetch('/data/data.json')
+            .then(respuesta => respuesta.json())
+            .then(datos => {
+                setTimeout(() => {
+                    setProductos(datos)
+                    setCargando(false)
+                }, 2000)
+            })
+            .catch(error => {
+                console.log('Error', error)
+                setCargando(false)
+                setError(true)
+            })
+
+    }, [])
+
+    const handleAddToCart = (producto) => {
+    const productInCart = cart.find((item) => item.id === producto.id);
+    if (productInCart) {
+
+      setCart(cart.map((item) => item.id === producto.id ? { ...item, quantity:item.quantity + 1 } : item));
+    } else {
+      setCart([...cart, { ...producto, quantity:1 }]);
+    }
+    };
+
+    const handleDeleteFromCart=(product)=>{
+
+    setCart(preVCart =>{
+      return preVCart.map(item => {
+        if(item.id=== product.id){
+          if(item.quantity > 1){
+            return {...item,quantity:item.quantity-1}
+          }else{
+            return null
+          }
+        }else{
+          return item
+        }
+      }).filter(item=> item != null)
+    })
+    };
+
+    return (
+        <CartContext.Provider 
+        value={
+
+            { cart, productos, cargando, error, handleAddToCart, handleDeleteFromCart, isAuthenticated  }
+        }>
+            {children}
+        </CartContext.Provider>
+    )
+}
---
+++ Pre-enterga-Ecommerce-TT-main/Pre-enterga-Ecommerce-TT-main/pre-entrega-tt/src/pages/Contacto.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from '../Componentes/estaticos/Header'
+import Footer from '../Componentes/estaticos/Footer'
+
+const Contacto = ({cart, borrarProducto}) => {
+  return (
+    <>
+      <Header borrarProducto={borrarProducto} cartItems={cart} />
+      <div className='contenedor__contacto' >
+        <h1 className="titulo__home">Hola soy Emma</h1>
+        <h2>Esta es mi pagina de contacto</h2>
+        <img src="/src/assets/img/Emma.png" alt="Foto de Emma" />
+        <form className="form" action="">
+          <label htmlFor="nombre">Nombre:</label>
+          <input type="text" id="nombre" name="nombre" required />
+
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+
+          <label htmlFor="mensaje">Mensaje:</label>
+          <textarea id="mensaje" name="mensaje" rows="4" required></textarea>
+
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default Contacto
