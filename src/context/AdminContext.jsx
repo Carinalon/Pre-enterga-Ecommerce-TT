d357e4c4887a6f81f1c17fc7a67dd81c11b3a6b9
alocarin <alocarin@gmail.com>
2025-07-21 19:37:22 -0300

    dv

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import Swal from "sweetalert2";
 
-export const AdminContext = createContext()
+export const AdminContext = createContext();
 
 export const AdminProvider = ({ children }) => {
   const [productos, setProductos] = useState([]);
@@ -9,7 +9,6 @@ export const AdminProvider = ({ children }) => {
   const [open, setOpen] = useState(false);
   const [seleccionado, setSeleccionado] = useState(null);
   const [openEditor, setOpenEditor] = useState(false);
-  
 
   useEffect(() => {
     fetch("/data/data.json")
@@ -29,7 +28,9 @@ export const AdminProvider = ({ children }) => {
 
   const cargarProductos = async () => {
     try {
-      const res = await fetch("/data/data.json");
+      const res = await fetch(
+        "https://683b8fd928a0b0f2fdc4efdc.mockapi.io/productos-ecommerce/productos"
+      );
       const data = await res.json();
       setProductos(data);
     } catch (error) {
@@ -39,20 +40,17 @@ export const AdminProvider = ({ children }) => {
 
   const agregarProducto = async (producto) => {
     try {
-      const respuesta = await fetch(
-        "/data/data.json",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(producto)
-        }
-      );
+      const respuesta = await fetch("/data/data.json", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(producto),
+      });
       if (!respuesta.ok) {
         throw new Error("Error al agregar producto");
       }
-      const data = await respuesta.json()
+      const data = await respuesta.json();
       Swal.fire({
         title: ":)!",
         text: "Producto agregado correctamente!",
@@ -67,42 +65,47 @@ export const AdminProvider = ({ children }) => {
 
   const actualizarProducto = async (producto) => {
     try {
-      const respuesta = await fetch(`${"/data/data.json"}/${producto.id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(producto)
-      });
+      const respuesta = await fetch(
+        `${"/https://683b8fd928a0b0f2fdc4efdc.mockapi.io/productos-ecommerce/productos"}/${
+          producto.id
+        }`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(producto),
+        }
+      );
       if (!respuesta.ok) throw Error("Error al actualizar el producto");
-      const data = await respuesta.json()
+      const data = await respuesta.json();
       alert("Producto actualizado correctamente");
-      setOpenEditor(false)
-      setSeleccionado(null)
-      cargarProductos()
+      setOpenEditor(false);
+      setSeleccionado(null);
+      cargarProductos();
     } catch (error) {
       console.log(error.message);
     }
   };
 
   const eliminarProducto = async (id) => {
-    const confirmar = window.confirm("Estas seguro de eliminar el producto?")
+    const confirmar = window.confirm("Estas seguro de eliminar el producto?");
     if (confirmar) {
       try {
         const respuesta = await fetch(
-          `"/data/data.json"${id}`,
+          `"https://683b8fd928a0b0f2fdc4efdc.mockapi.io/productos-ecommerce/productos"${id}`,
           {
             method: "DELETE",
-          })
+          }
+        );
         if (!respuesta.ok) throw Error("Error al eliminar");
-        
+
         Swal.fire({
           title: ":(!",
           text: "Producto Eliminado correctamente!",
           icon: "error",
         });
-        cargarProductos()
-
+        cargarProductos();
       } catch (error) {
         alert("Hubo un problema al eliminar el producto");
       }
@@ -110,7 +113,8 @@ export const AdminProvider = ({ children }) => {
   };
 
   return (
-    <AdminContext.Provider value={{
+    <AdminContext.Provider
+      value={{
         productos,
         loading,
         open,
